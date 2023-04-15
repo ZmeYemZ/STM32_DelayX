bbdb461861103b23ea28961d67be367d07387830
ZmeYemZ <70698550+ZmeYemZ@users.noreply.github.com>
2023-04-15 17:15:33 +0300

    patient 0x00

---
+++ DelayX.h
@@ -0,0 +1,60 @@
+#ifndef SRC_SRC_CPP_DELAYX_H_
+#define SRC_SRC_CPP_DELAYX_H_
+
+#include <inttypes.h>
+
+#if defined(__cplusplus)
+    class CDelayX
+    {
+    private:
+        uint32_t m_uDuration;
+        uint32_t m_uTickPrev;
+        uint32_t m_uTickNext;
+    public:
+        CDelayX(uint32_t uDuration)
+        :m_uDuration(uDuration)
+        {
+            m_uTickPrev = HAL_GetTick();
+            m_uTickNext = m_uTickPrev + m_uDuration;
+        }
+
+        bool Huh()
+        {
+            const uint32_t uTick = HAL_GetTick();
+
+            if(m_uTickPrev < m_uTickNext)
+            {
+                if(uTick >= m_uTickPrev && uTick < m_uTickNext) return true;
+            }
+            else
+            {
+                if(uTick >= m_uTickPrev || uTick < m_uTickNext) return true;
+            }
+
+            m_uTickPrev = m_uTickNext;
+            m_uTickNext += m_uDuration;
+            return false;
+        }
+    };
+#else
+    static bool DelayX(uint32_t* puTickPrev, uint32_t uDuration)
+    {
+        const uint32_t uTick = HAL_GetTick();
+        const uint32_t uTickPrev = *puTickPrev;
+        const uint32_t uTickNext = uTickPrev + uDuration;
+
+        if(uTickPrev < uTickNext)
+        {
+            if(uTick >= uTickPrev && uTick < uTickNext) return true;
+        }
+        else
+        {
+            if(uTick >= uTickPrev || uTick < uTickNext) return true;
+        }
+
+        *puTickPrev = uTickNext;
+        return false;
+    }
+#endif
+
+#endif /* SRC_SRC_CPP_DELAYX_H_ */
